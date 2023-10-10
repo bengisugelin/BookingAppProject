7e8c8f5204d3224f26e818e2340482b8f79ca796
bengisu <bengisugelin@gmail.com>
2023-10-10 14:01:37 -0700

    featuredProperties were added

---
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./featuredProperties.css"
+
+
+const FeaturedProperties = ()=> {
+    return(
+        <div className="fp">
+            <div className="fpItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/289317322.webp?k=e9cd1c83d33881344f3b30c1c69c151e6c7209dcdf2f5da7541d4e9476707d11&o=" alt="" className="fpImg" />
+                <span className="fpName">Spirit Lodge at SilverStar</span>
+                <span className="fpCity">Canada, Vernon</span>
+                <span className="fpPrice">Starting from <b>CAD 150</b></span>
+                <div className="fpRating">
+                    <button>9.8</button>
+                    <span>Exceptional</span>
+                </div>
+            </div>
+            <div className="fpItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/289317322.webp?k=e9cd1c83d33881344f3b30c1c69c151e6c7209dcdf2f5da7541d4e9476707d11&o=" alt="" className="fpImg" />
+                <span className="fpName">Spirit Lodge at SilverStar</span>
+                <span className="fpCity">Canada, Vernon</span>
+                <span className="fpPrice">Starting from <b>CAD 150</b></span>
+                <div className="fpRating">
+                    <button>9.8</button>
+                    <span>Exceptional</span>
+                </div>
+            </div>
+            <div className="fpItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/289317322.webp?k=e9cd1c83d33881344f3b30c1c69c151e6c7209dcdf2f5da7541d4e9476707d11&o=" alt="" className="fpImg" />
+                <span className="fpName">Spirit Lodge at SilverStar</span>
+                <span className="fpCity">Canada, Vernon</span>
+                <span className="fpPrice">Starting from <b>CAD 150</b></span>
+                <div className="fpRating">
+                    <button>9.8</button>
+                    <span>Exceptional</span>
+                </div>
+            </div>
+
+            
+        </div>
+    )
+}
+export default FeaturedProperties;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,6 +4,8 @@ import Navbar from "../../components/navbar/Navbar.jsx";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
 import PropertyList from "../../components/propertyList/PropertyList.jsx";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties.jsx";
+
 
 const Home = ()=> {
     return(
@@ -14,6 +16,9 @@ const Home = ()=> {
                 <Featured/>
                 <h1 className="homeTitle">Browse by property type</h1>
                 <PropertyList/>
+                <h1 className="homeTitle">Homes guests love</h1>
+                <FeaturedProperties/>
+                
             </div>
         </div>
     )
